d95b57c6b92f0d541a3bbeb366946636b9f883ce
polaris218 <software.dev0218@gmail.com>
2020-05-13 05:24:27 +0500

    Stripe payment confirmation.

--- accounts/STRIPE_payments.py
+++ accounts/STRIPE_payments.py
@@ -11,7 +11,7 @@ class StripePayment(object):
 
     def bid_payment(self, bid, request):
         charge = stripe.PaymentIntent.create(
-            amount=bid.bid_amount * 100,
+            amount=int(bid.bid_amount * 100),
             currency='usd',
             customer=request.user.stripe_customer_id,
             payment_method=request.user.stripe_payment_method,
@@ -24,8 +24,8 @@ class StripePayment(object):
             bid.save()
             bid_payment = BidPayment.objects.create(amount=bid.bid_amount,
                                                     admin_url=admin_funding_source,
-                                                    buyer_url=charge.to_dict().get('receipt_url'),
-                                                    success_url=charge.to_dict().get('receipt_url'), bid=bid,
+                                                    buyer_url=charge.to_dict().get('charges').get('data')[0].get('receipt_url'),
+                                                    success_url=charge.to_dict().get('charges').get('data')[0].get('receipt_url'), bid=bid,
                                                     payment_method=BidPayment.STRIPE)
             return bid_payment
         return None
