9954e18a4f5c17df7244bfd0b8e3ea6e11b456ac
polaris218 <software.dev0218@gmail.com>
2020-11-20 20:08:14 -0500

    Allow all hosts for test

--- Payouttt/settings.py
+++ Payouttt/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '7!wg%_&mmsf44+x1d0&e7#k_!zv+z+e6l(o)v4p+v57$l7(lh&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['quickkicks.app', 'quickkicks.com', 'quickkicks.io']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
