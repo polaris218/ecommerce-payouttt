bbc06970308ae83318a9bc8adb064171979a7adb
polaris218 <software.dev0218@gmail.com>
2021-04-01 07:40:07 +0300

    initial commit

--- Payouttt/settings.py
+++ Payouttt/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'Payouttt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-      'default': {
-          'ENGINE': 'django.db.backends.postgresql',
-          'NAME': os.getenv('POSTGRES_DB', 'Payouttt_portal'),
-          'USER': os.getenv('POSTGRES_USER', 'postgres'),
-          'PASSWORD': os.getenv('POSTGRES_PASSWORD', '1234'),
-          'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
-       #   'PORT': os.getenv('POSTGRES_PORT', 5432)
-      }
-  }
+# DATABASES = {
+#       'default': {
+#           'ENGINE': 'django.db.backends.postgresql',
+#           'NAME': os.getenv('POSTGRES_DB', 'Payouttt_portal'),
+#           'USER': os.getenv('POSTGRES_USER', 'postgres'),
+#           'PASSWORD': os.getenv('POSTGRES_PASSWORD', '1234'),
+#           'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
+#        #   'PORT': os.getenv('POSTGRES_PORT', 5432)
+#       }
+#   }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
